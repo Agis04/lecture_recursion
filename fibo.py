3ca67109ce4c1973ecf6a4e30860eea32402b4ad
Agata Vyslouzilova <agata.vyslouzilova@seznam.cz>
2024-04-22 12:26:06 +0200

    Prvni revize

--- fibo.py
+++ fibo.py
@@ -1,5 +1,14 @@
+def recursive_nth_fibo(n):
+    if n <= 1:
+        return n
+    else:
+        return recursive_nth_fibo(n - 1) + recursive_nth_fibo(n - 2)
+
 
 def main():
+    n = int(input("Zadejte kolik prvku Fibonacciho prvku vas zajima: "))
+    seq = [recursive_nth_fibo(num) for num in range(0, n)]
+    print(seq)
     pass
 
 
